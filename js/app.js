d12a119308aadb3dbda474ed2e3427df8bfaa692
Abidur Rahman Chowdhury <abid43iiuc@gmail.com>
2022-03-10 22:29:02 +0600

    fixed bug 1 click on love button not work

--- js/app.js
+++ js/app.js
@@ -16,7 +16,9 @@ const isLiked = (id) => {
 };
 
 const addToLiked = (id) => {
-    likedPostsId.plus(id); 
+  // console.log(id);
+  //  bug 1 fixed 
+    likedPostsId.push(id); 
     showPosts(posts);
 };
 
